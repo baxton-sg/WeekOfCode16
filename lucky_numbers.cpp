de99c2f2cc4b673e5cafc44d35600521b4c33a52
Maxim Alekseykin <baxton-sg@users.noreply.github.com>
2015-07-09 09:19:37 +0800

    v3

--- lucky_numbers.cpp
+++ lucky_numbers.cpp
@@ -1,8 +1,6 @@
-
-
 #include <iostream>
-#include <vector>
-#include <unordered_set>
+#include <cstring>
+#include <set>
 #include <algorithm>
 #include <cmath>
 
@@ -16,15 +14,56 @@ const int MAX_NUMBERS = 100 * 100 * 100;
 
 
 
+struct data {
+    char num[MAX_NUMBERS];
+    int size;
 
-size_t fill_numbers(int x, int y, int z, vector<int>& vec) {
+    data() {}
+    data(const data& rhs) {
+        size = rhs.size;
+        memcpy(num, rhs.num, size);
+    }
+
+    char& operator[] (size_t i) { return num[i];}
+};
+
+bool operator<(const data& d1, const data& d2) {
+    if (d1.size < d2.size)
+        return true;
+
+    if (d1.size > d2.size)
+        return false;
+
+    for (int i = 0; i < d1.size; ++i)
+        if (d1.num[i] > d2.num[i])
+            return false;
+
+    return true;
+}
+
+bool operator==(const data& d1, const data& d2) {
+    if (d1.size != d2.size)
+        return false;
+
+    for (int i = 0; i < d1.size; ++i)
+        if (d1.num[i] != d2.num[i])
+            return false;
+
+    return true;
+}
+
+
+
+size_t fill_numbers(int x, int y, int z, data& vec) {
     size_t size = x + y + z;
     size_t cur = 0;
 
+    vec.size = size;
+
     while (x && y && z) {
-        vec[cur+0] = 4;
-        vec[cur+1] = 5;
-        vec[cur+2] = 6;
+        vec.num[cur+0] = 4;
+        vec.num[cur+1] = 5;
+        vec.num[cur+2] = 6;
 
         cur += 3;
 
@@ -34,17 +73,17 @@ size_t fill_numbers(int x, int y, int z, vector<int>& vec) {
     }
 
     while (x) {
-        vec[cur++] = 4;
+        vec.num[cur++] = 4;
         --x;
     }
 
     while (y) {
-        vec[cur++] = 5;
+        vec.num[cur++] = 5;
         --y;
     }
 
     while (z) {
-        vec[cur++] = 6;
+        vec.num[cur++] = 6;
         --z;
     }
 
@@ -52,45 +91,72 @@ size_t fill_numbers(int x, int y, int z, vector<int>& vec) {
 }
 
 
-int get_num(vector<int>& numbers, size_t size) {
+int get_num(const data& numbers) {
     int result = 0;
-    for (size_t i = 0; i < size; ++i)
-        result = result * 10 + numbers[i];
+    for (size_t i = 0; i < numbers.size; ++i)
+        result = result * 10 + numbers.num[i];
 
     return result;
 }
 
 
-void get_sum(vector<int>& numbers, size_t size, int init_val, unordered_set<int>& memo, size_t current, ULONG& sum) {
 
-    if (current >= size)
+
+
+
+ULONG add(int num, int num_zeros, ULONG& sum) {
+    if (num_zeros <= 8) {
+        sum += num * pow(10, num_zeros);
+
+        return sum % MASK;
+    }
+
+    int mul = num_zeros / 8; // 8 <= 100M
+    int deg = num_zeros - mul * 8;
+    int val = num * pow(10, deg) * mul * 999307;
+
+    sum += val;
+
+    return sum % MASK;
+}
+
+
+ULONG add(const data& num, ULONG& sum) {
+    int num_zeros = num.size - 1;
+
+    for (int i = 0; i < num.size; ++i) {
+        int n = num.num[i];
+        sum = add(n, num_zeros--, sum);
+    }
+
+    return sum % MASK;
+}
+
+
+void get_sum(data& numbers, set<data>& memo, size_t current, ULONG& sum) {
+
+    if (current >= numbers.size)
         return;
 
 
-    get_sum(numbers, size, init_val, memo, current+1, sum);
+    get_sum(numbers, memo, current+1, sum);
 
 
-    for (size_t i = current+1; i < size; ++i) {
+    for (size_t i = current+1; i < numbers.size; ++i) {
         if (numbers[current] == numbers[i])
             continue;
 
+        swap(numbers[current], numbers[i]);
 
-        int p1 = pow(10, size - current - 1),
-            p2 = pow(10, size - i - 1);
-        int tmp = init_val - (numbers[current] * p1) - (numbers[i] * p2) 
-                           + (numbers[current] * p2) + (numbers[i] * p1);
-
-
-
-        if (memo.end() == memo.find(tmp)) {
-            memo.insert(tmp);
-            sum += tmp;
+        if (memo.end() == memo.find(numbers)) {
+            memo.insert(numbers);
+            sum = add(numbers, sum);
         
 
-            swap(numbers[current], numbers[i]);
-            get_sum(numbers, size, tmp, memo, current+1, sum);
-            swap(numbers[current], numbers[i]);
+            get_sum(numbers, memo, current+1, sum);
         }
+
+        swap(numbers[current], numbers[i]);
     }
 
 }
@@ -99,8 +165,8 @@ void get_sum(vector<int>& numbers, size_t size, int init_val, unordered_set<int>
 int solve(int x, int y, int z) {
     ULONG sum = 0;
 
-    vector<int> numbers(MAX_NUMBERS, 0);
-    unordered_set<int> memo;
+    data numbers;
+    set<data> memo;
 
     for (int i = 0; i <= x; ++i) {
         for (int j = 0; j <= y; ++j) {
@@ -108,11 +174,10 @@ int solve(int x, int y, int z) {
                 size_t size = fill_numbers(i, j, k, numbers);
 
                 if (size) {
-                     int n = get_num(numbers, size);
-                     memo.insert(n);
-                     sum += n;
+                     memo.insert(numbers);
+                     sum = add(numbers, sum);
 
-                     get_sum(numbers, size, n, memo, 0, sum);
+                     get_sum(numbers, memo, 0, sum);
                 }
             }
         }
